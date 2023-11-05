9c21b766451c8f7b64d6a97b9be35b7d6b9be90a
Matheus Olegario <mgolegario@live.com>
2023-11-05 11:12:28 -0300

    example1 added and formatted

--- exercicios_js_pw_13_11/example1.js
+++ exercicios_js_pw_13_11/example1.js
@@ -2,11 +2,19 @@ function exibeSoma() {
     var valorInserido = parseInt(document.getElementById('inputEx1').value);
     var valorEx1 = document.getElementById('exitEx1');
     var somaEx1 = 0;
-
+    valorEx1.innerHTML="Os números pares até "+ valorInserido+ " são: ";
     for (let i = 1; i <= valorInserido; i++) {
+        if (i%2==0) {
+            if (i != valorInserido ){
         valorEx1.innerHTML += i + ', ';
+            }else{
+                valorEx1.innerHTML += i + '. ';
+            }
         somaEx1 = somaEx1 + i;
+        
     }
-    valorEx1.innerHTML += 'e a soma desses valores é ' + somaEx1 + '.';
+    }
+
+    valorEx1.innerHTML += '<br>A soma desses valores é ' + somaEx1 + '.';
 
 }
